8adaf403b1c492f03d293ef5597cb23e4311f8eb
Anton Kochkov <anton.kochkov@gmail.com>
2011-04-25 21:29:44 +0400

    bugfix

--- file_parser.py
+++ file_parser.py
@@ -7,7 +7,7 @@ def lspci_parse(text):
 	devices = []
 
 	pci_devnum = re.compile("(?m)^(?P<devnum>[\dA-Fa-f]{2}:[\dA-Fa-f]{2}.[\dA-Fa-f])(?P<class>.*)\[(?P<some>[\dA-Fa-f]{4})\]:(?P<name>.*)\[(?P<id>[\dA-Fa-f]{4}:[\dA-Fa-f]{4})\]")
-	pci_space = re.compile("(?m)^(?P<line>[\dA-Fa-f]{2,4}):(?P<registers>( [\dA-Fa-f]{2}){16})$")
+	pci_space = re.compile("(?m)^(?P<line>[\dA-Fa-f]{2,4}):(?P<registers>(?: [\dA-Fa-f]{2}){16})$")
 
 	head_group_name_by_index = dict( [ (v, k) for k, v in pci_devnum.groupindex.items() ])
 	space_group_name_by_index = dict( [ (v, k) for k, v in pci_space.groupindex.items() ])
@@ -24,13 +24,13 @@ def lspci_parse(text):
 					device.bus = int(group[0:2], 16)
 					device.device = int(group[3:5], 16)
 					device.function = int(group[6:7], 16)
-					print("device number: {0} : {1} . {2}".format(device.bus, device.device, device.function))
+					# print("device number: {0} : {1} . {2}".format(device.bus, device.device, device.function))
 				if head_group_name_by_index[group_index+1] == "id" :
 					device.id = group.split(":")
-					print("device id : {0}".format(device.id))
+					# print("device id : {0}".format(device.id))
 				if head_group_name_by_index[group_index+1] == "name" :
 					device.name = group
-					print("device name : {0}".format(device.name))
+					# print("device name : {0}".format(device.name))
 				
 			#	print("{0}: {1}".format(head_group_name_by_index[group_index+1], group))
 
@@ -38,23 +38,24 @@ def lspci_parse(text):
 	
 	# fill registers values
 
-	lines_count = 0;
-	device_count = 0;
+	line = 0;
+	device_count = -1;
 	
 	pci_regs = pci_space.finditer(text)
 
 	for match in pci_regs :
 		for group_index, group in enumerate(match.groups()) :
 			if group :
-				# print(group)
-				if space_group_name_by_index[group_index+1] == "registers" :
-					devices[device_count].space += group.split(" ")
-					print(devices[device_count].space)
 				if space_group_name_by_index[group_index+1] == "line" :
-					lines_count += 1
+					line += 1
 					if group == "00":
 						device_count += 1
-
+						print(" ------------ Register DATA read: ------------")
+						print("		Name: {0} ID: {1} {2}".format(devices[device_count].name, devices[device_count].id, devices[device_count].device))
+				if space_group_name_by_index[group_index+1] == "registers" :
+					devices[device_count].space.append(group[1:].split(" "))
+					#print(devices[device_count].space[line])
+				
 lspci_log = open("lspci.log", "r")
 lspci = lspci_log.read()
 lspci_parse(lspci)
